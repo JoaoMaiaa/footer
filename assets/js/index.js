5144c69aa02ec9ac7b707a76586722b264d7ece3
Joao Maia <15joaomaia@gmail.com>
2020-12-14 11:13:58 -0300

    sections-bootstrap-4

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+$(()=>{
+    $('.img-slider').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+});
